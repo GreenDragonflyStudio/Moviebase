33f102abd4bc016164f82a26acdd8bd625f8187d
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-05-22 20:39:16 +0700

    added DAL

---
+++ Moviebase.DAL/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Moviebase.DAL
+{
+    public interface IRepository<T, in TKey> where T : class
+    {
+        T GetById(TKey id);
+        void Delete(TKey id);
+        void Insert(T entity);
+        void Update(T entity);
+    }
+}
---
+++ Moviebase.DAL/ISeeder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Moviebase.DAL
+{
+    public interface ISeeder<out T> where T : class
+    {
+        IEnumerable<T> Seed();
+    }
+}
