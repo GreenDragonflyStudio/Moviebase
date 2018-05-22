858d6d38ea9101a74c01475db5d100ba28e39f90
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-05-22 20:39:16 +0700

    added DAL

--- Moviebase.Core/Models/MovieItem.cs
+++ Moviebase.Core/Models/MovieItem.cs
@@ -15,9 +15,7 @@ namespace Moviebase.Models
     }
     public class Genre
     {
-        public int GenreId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        
         public List<MovieItem> Albums { get; set; }
     }
      
---
+++ Moviebase.DAL/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Moviebase.DAL.Entities
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int[] GenreIds { get; set; }
+        public string Synopsis { get; set; }
+
+        public float Rating { get; set; }
+        public bool Synced { get; set; }
+        public string ImageLocation { get; set; }
+    }
+}
