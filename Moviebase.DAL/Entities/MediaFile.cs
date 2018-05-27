539637c535a8863fac27d767e0107407ae47d187
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-05-27 19:21:03 +0700

    merge entity

--- Moviebase.DAL/Entities/MediaFile.cs
+++ Moviebase.DAL/Entities/MediaFile.cs
@@ -5,32 +5,26 @@ namespace Moviebase.DAL.Entities
 {
     public class MediaFile
     {
-        public string Hash { get; set; }
-        public long Length { get; set; }
+        public int Id { get; set; }
 
-        public DateTime LastModifiedUtc { get; set; }
-        public string Path { get; set; }
+        public string Hash { get; set; }
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        public DateTime LastSync { get; set; }
+        public string FullPath { get; set; }
+        public bool Synced { get; set; }
 
         public MediaFile()
         {
         }
-
-        public static MediaFile Create(string filePath)
-        {
-            var fileInfo = new FileInfo(filePath);
-            return new MediaFile
-            {
-                Path = filePath,
-                LastModifiedUtc = fileInfo.LastWriteTimeUtc,
-                Length = fileInfo.Length
-            };
-        }
-
+        
         public bool Equals(MediaFile other)
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return string.Equals(Hash, other.Hash) && LastModifiedUtc.Equals(other.LastModifiedUtc) && string.Equals(Path, other.Path);
+            return string.Equals(Hash, other.Hash) &&  string.Equals(FullPath, other.FullPath);
         }
 
         public override bool Equals(object obj)
@@ -46,8 +40,7 @@ namespace Moviebase.DAL.Entities
             unchecked
             {
                 var hashCode = (Hash != null ? Hash.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ LastModifiedUtc.GetHashCode();
-                hashCode = (hashCode * 397) ^ (Path != null ? Path.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (FullPath != null ? FullPath.GetHashCode() : 0);
                 return hashCode;
             }
         }
