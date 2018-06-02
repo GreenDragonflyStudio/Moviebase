0d91fa197bb1066d9556adb8ff32341127a9e55d
Fahmi Rasyid <ufarasfa@gmail.com>
2018-06-03 00:35:31 +0700

    Minor UI Tweak 0.2.1

---
+++ Moviebase.Services/Title/TerkaTitleProvider.cs
@@ -0,0 +1,22 @@
+﻿using Moviebase.Services.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Moviebase.Services.Entities;
+
+namespace Moviebase.Services.Title
+{
+    public class TerkaTitleProvider : ITitleProvider
+    {
+        public void AddProvider(ITitleProvider provider)
+        {
+            throw new NotSupportedException("Could not add provider to this instance.");
+        }
+
+        public async Task<GuessTitle> GuessTitle(string filename)
+        {
+            return null; // TODO : Future Implementation
+        }
+    }
+}
